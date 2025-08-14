749026535da1e84b4168031ee9e48947fa8648f9
agilecharl <agilecharl@gmail.com>
2025-08-14 17:39:43 +0200

    Initial commit

---
+++ apps/react-repo/app/app.tsx
@@ -0,0 +1,11 @@
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="@react-repo/react-repo" />
+    </div>
+  );
+}
+
+export default App;
